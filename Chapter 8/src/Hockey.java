58bd0eaba53d1ed55898c236057b655bb3ebbc5f
904269504 <904269504@L815WD150047.edu.cbe.ab.ca>
2022-03-04 10:26:56 -0700

    deciamls?

--- Chapter 8/src/Hockey.java
+++ Chapter 8/src/Hockey.java
@@ -5,13 +5,16 @@ public class Hockey {
 	{
 		Puck firstPuck = new Puck(3.5);
 		
+	    NumberFormat dec = NumberFormat;
+	 
+		
 		System.out.println("Does the pull from Git Hub work? yes!");
 		System.out.println("  Weight of puck is " + firstPuck.getWeight());
 		System.out.println("  Divison of game  is " + firstPuck.getDivision());
 		System.out.println("  Radius of Puck is " + firstPuck.getRadius());
 		System.out.println("  Thickness of Puck is " + firstPuck.getThickness());
-		//System.out.println("  Area of Puck is " + firstPuck.area().format);
-		//System.out.println("  Volume of Puck is " + NumberFormat.firstPuck.volume());
+		System.out.println("  Area of Puck is " + dec.format(firstPuck.area()));
+		System.out.println("  Volume of Puck is " + dec.format(firstPuck.volume()));
 		
 	}
 
