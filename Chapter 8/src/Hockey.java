3ca5ff2f840d305bc261db47c539f6885de2cb60
noodl <noodl@192.168.1.107>
2022-03-03 21:06:08 -0700

    does it work?

--- Chapter 8/src/Hockey.java
+++ Chapter 8/src/Hockey.java
@@ -11,7 +11,7 @@ public class Hockey {
 		System.out.println("  Thickness of Puck is " + firstPuck.getThickness());
 		//System.out.println("  Area of Puck is " + firstPuck.area().format);
 		//System.out.println("  Volume of Puck is " + NumberFormat.firstPuck.volume());
-
+		System.out.println("Does the pull from GitHub work?");
 	}
 
 }
