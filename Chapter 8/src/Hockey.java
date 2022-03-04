e78c2951e31f8f3ac8732774ffc1382f0da4cc10
noodl <noodl@192.168.1.107>
2022-03-03 21:08:39 -0700

    does it work???

--- Chapter 8/src/Hockey.java
+++ Chapter 8/src/Hockey.java
@@ -5,13 +5,14 @@ public class Hockey {
 	{
 		Puck firstPuck = new Puck(3.5);
 		
+		System.out.println("Does the pull from Git Hub work?");
 		System.out.println("  Weight of puck is " + firstPuck.getWeight());
 		System.out.println("  Divison of game  is " + firstPuck.getDivision());
 		System.out.println("  Radius of Puck is " + firstPuck.getRadius());
 		System.out.println("  Thickness of Puck is " + firstPuck.getThickness());
 		//System.out.println("  Area of Puck is " + firstPuck.area().format);
 		//System.out.println("  Volume of Puck is " + NumberFormat.firstPuck.volume());
-		System.out.println("Does the pull  from Git Hub work?");
+		
 	}
 
 }
