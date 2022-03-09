c6580132c17669b85d20a1f0288b2cc62a8164e1
904269504 <904269504@L815WD150047.edu.cbe.ab.ca>
2022-03-09 11:50:33 -0700

    aaaaaaa

--- Chapter 8/src/bankAccounts/PersonalAccount.java
+++ Chapter 8/src/bankAccounts/PersonalAccount.java
@@ -2,9 +2,17 @@ package bankAccounts;
 
 public class PersonalAccount extends Account
 {
-
-	if(balance<100){
-	balance -= 2;	
+	private Customer cust;
+	private double balance;
+	public PersonalAccount(double bal, String fName, String lName, String str, String city, String st, String zip) 
+	{
+		super(bal,fName,lName,str,city,st,zip);
+		cust = new Customer(fName, lName, str, city, st, zip);
+		if(balance<100)
+		{
+			bal -= 2;	
+		}	
 	}
+	
 	
 }
