735f26c30d1a5915354f92a7d220aa2e3a96beef
904269504 <904269504@L815WD150047.edu.cbe.ab.ca>
2022-03-09 11:02:41 -0700

    aaaaaaaaaaa

---
+++ Chapter 8/src/uEmployee/Faculty/Staff/Faculty.java
@@ -0,0 +1,12 @@
+package uEmployee.Faculty.Staff;
+
+public class Faculty extends uEmployee
+{
+	public String department;
+	
+	public String getDepartment() 
+	{
+		return department;	
+	}
+	
+}
---
+++ Chapter 8/src/uEmployee/Faculty/Staff/Staff.java
@@ -0,0 +1,11 @@
+package uEmployee.Faculty.Staff;
+
+public class Staff extends uEmployee
+{
+	public double cakes;
+	private String jobtitle;
+	
+	public String getTitle(){
+	return jobtitle;	
+	}
+}
