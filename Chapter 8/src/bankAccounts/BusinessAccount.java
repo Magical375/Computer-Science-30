9dc93680d8632823ac7dd3f64501cf557c7bcb87
904269504 <904269504@L815WD150047.edu.cbe.ab.ca>
2022-03-09 11:53:20 -0700

    aaaaaaaaaa

--- Chapter 8/src/bankAccounts/BusinessAccount.java
+++ Chapter 8/src/bankAccounts/BusinessAccount.java
@@ -2,5 +2,12 @@ package bankAccounts;
 
 public class BusinessAccount extends Account
 {
-
+	private Customer cust;
+	private double balance;
+ public BusinessAccount(double bal, String fName, String lName, String str, String city, String st, String zip) 
+ {
+	 super(bal,fName,lName,str,city,st,zip);
+	cust = new Customer(fName, lName, str, city, st, zip);
+	 
+ }
 }
