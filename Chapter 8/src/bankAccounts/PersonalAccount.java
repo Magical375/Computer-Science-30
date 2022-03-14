41e66ca7514630dc9b072437717dd5bdb010212e
904269504 <904269504@L815WD150047.edu.cbe.ab.ca>
2022-03-14 11:42:41 -0600

    aaaaaaaaaaa

--- Chapter 8/src/bankAccounts/PersonalAccount.java
+++ Chapter 8/src/bankAccounts/PersonalAccount.java
@@ -17,7 +17,6 @@ public class PersonalAccount extends Account
 	 		if(super.getBalance() < 100)
 	 		{
 	 			super.withdrawal(amt);
-	 			System.out.println("Not enough money in account. You were charged two dollars.");
 	 		}
 	 	} 
 		}
