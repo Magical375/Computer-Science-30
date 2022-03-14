9130063b5888a355ceb208b2d78b5397f6d444f5
904269504 <904269504@L815WD150047.edu.cbe.ab.ca>
2022-03-14 11:06:10 -0600

    aaaaaaaaaaaa

--- Chapter 8/src/bankAccounts/PersonalAccount.java
+++ Chapter 8/src/bankAccounts/PersonalAccount.java
@@ -2,16 +2,17 @@ package bankAccounts;
 
 public class PersonalAccount extends Account
 {
-	public PersonalAccount(double bal, String fName, String lName, String str, String city, String st, String zip) 
+	
+	public PersonalAccount(double bal, String fName, String lName, String str, String city, String st, String zip,double amt) 
 	{
-		super(bal,fName,lName,str,city,st,zip);
+		super(bal,fName,lName,str,city,st,zip,amt);
 	}
 	
 	
-	public void withdraw(double amount, double bal) {
+	public void withdraw(double amt, double bal) {
 		
 		if (bal <= 100) {
-	 		bal-= super.amount;
+	 		bal-= super.amt;
 	 	} else {
 	 		System.out.println("Not enough money in account.");
 	 	}	
