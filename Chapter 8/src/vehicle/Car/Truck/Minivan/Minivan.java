0f831977e48c8fd593c9eaa63f506d67260f1d52
904269504 <904269504@L815WD150047.edu.cbe.ab.ca>
2022-03-15 10:19:58 -0600

    aaaaaaaaaaaaa

--- Chapter 8/src/vehicle/Car/Truck/Minivan/Minivan.java
+++ Chapter 8/src/vehicle/Car/Truck/Minivan/Minivan.java
@@ -2,5 +2,8 @@ package vehicle.Car.Truck.Minivan;
 
 public class Minivan extends Vehicle
 {
-
+	public Minivan(String name, String colour, double seats) 
+	{
+		
+	}
 }
--- Chapter 8/src/vehicle/Car/Truck/Minivan/Truck.java
+++ Chapter 8/src/vehicle/Car/Truck/Minivan/Truck.java
@@ -2,5 +2,8 @@ package vehicle.Car.Truck.Minivan;
 
 public class Truck  extends Vehicle
 {
-
+	public Truck(String name, String colour, double seats) 
+	{
+		
+	}
 }
