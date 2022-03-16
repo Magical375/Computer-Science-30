e3e1ca58baea662afe90bc3b07060e5f47f9e45f
904269504 <904269504@L815WD150047.edu.cbe.ab.ca>
2022-03-16 11:21:25 -0600

    aaaaaaaa

--- Chapter 8/src/vehicle/Car/Truck/Minivan/Minivan.java
+++ Chapter 8/src/vehicle/Car/Truck/Minivan/Minivan.java
@@ -2,13 +2,13 @@ package vehicle.Car.Truck.Minivan;
 
 public class Minivan extends Vehicle
 {
-	public int stickers;
+	public boolean movieplayer;
 	public Minivan(String n, String c, double s, String st,String plate) 
 	{
 		super(n,c,s,st,plate);
 		st = "medium";
-		stickers = 3;
+		movieplayer = true;
 		s = 8;
-		System.out.println("Storage is "+st+"Number of stickers"+stickers+"Max seats of a minivan is "+s);	
+		System.out.println("Storage is "+st+"Movieplayer is minivan "+movieplayer+"Max seats of a minivan is "+s);	
 	}
 }
