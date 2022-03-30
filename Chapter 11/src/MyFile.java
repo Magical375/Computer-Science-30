6bfe93bb90c4cade88f18862b802b72babd89539
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-03-30 11:33:37 -0600

    ccccccccccccccccc

--- Chapter 11/src/MyFile.java
+++ Chapter 11/src/MyFile.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.io.*;
 public class MyFile {
 
 	public static void main(String[] args) 
@@ -6,6 +7,12 @@ public class MyFile {
 		Scanner input = new Scanner(System.in);	
 		System.out.println("Please enter name of a file");
 		String Name= input.nextLine();
+	File textFile = new File("c:\\temp\\Name.txt");	
+	if (textFile.exists()) {
+		System.out.println("File exists.");	
+	}else {
+		System.out.println("File does not exist.");		
+	}
 	}
 
 }
