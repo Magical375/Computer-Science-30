5350dbc0642360f12dae36bddce71417e655bc55
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-03-30 11:52:15 -0600

    sa

--- Chapter 11/src/MyFile.java
+++ Chapter 11/src/MyFile.java
@@ -5,12 +5,13 @@ public class MyFile {
 	public static void main(String[] args) 
 	{
 		Scanner input = new Scanner(System.in);	
-		System.out.println("Please enter name of a file");
+		System.out.println("Please enter the directory of a file");// choices that work "../Chapter 11/src/name.txt" or "C:\Users\904269504\git\Computer-Science-30\Chapter 11\src\name.txt"
 		String Name= input.nextLine();
-	File textFile = new File("c:\\temp\\Name.txt");	
+	File textFile = new File(Name);	
 	if (textFile.exists()) {
 		System.out.println("File exists.");	
-	}else {
+	}
+	else{
 		System.out.println("File does not exist.");		
 	}
 	}
