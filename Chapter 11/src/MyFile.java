8a0c90aecf374f82f1b1ea5c9d733c6c17a9152d
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-03-30 11:30:12 -0600

    zzzzzzzzzzzzzzzz

--- Chapter 11/src/MyFile.java
+++ Chapter 11/src/MyFile.java
@@ -1,9 +1,11 @@
-
+import java.util.Scanner;
 public class MyFile {
 
-	public static void main(String[] args) {
-		
-
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);	
+		System.out.println("Please enter name of a file");
+		String Name= input.nextLine();
 	}
 
 }
