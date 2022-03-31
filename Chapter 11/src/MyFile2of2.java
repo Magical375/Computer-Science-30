1c9b2f84041e76f2e75c2fa0d8256f61c244d70f
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-03-31 10:01:03 -0600

    ggggggggggggg

--- Chapter 11/src/MyFile2of2.java
+++ Chapter 11/src/MyFile2of2.java
@@ -4,33 +4,35 @@ public class MyFile2of2 {
 	public static void main(String[] args) 
 	{
 		Scanner input = new Scanner(System.in);	
-		System.out.println("Please enter the directory of a file");// choices that work "../Chapter 11/src/name.txt" or "C:\Users\904269504\git\Computer-Science-30\Chapter 11\src\name.txt"
+		System.out.println("Please enter the directory of a file");
+		// choices that work "../Chapter 11/src/[PICK NAME HERE WITH .txt]" or "C:\Users\904269504\git\Computer-Science-30\Chapter 11\src\[PICK NAME HERE WITH .txt]"
 		String Name= input.nextLine();
 	File textFile = new File(Name);	
-	if (textFile.exists()) 
-	{
-		System.out.println("File exists.");	
-	}
-	else
-	{
-		try 
-		{
-			textFile.createNewFile();
-			System.out.println("File has been created.");
-		}
-		catch (IOException e) 
-		{
-			System.out.println("File could not be created.");
-			System.err.println("IOException: " + e.getMessage());
-		}
-		
-	}
 	
+	//if (textFile.exists()) 
+	//{
+	//	System.out.println("File exists.");	
+	//}
+	//else
+	//{
+		//try 
+		//{
 			
-	System.out.println("Keep or delete (type D) file? ");
+		//}
+		//catch (IOException e) 
+		//{
+		//	System.out.println("File could not be created.");
+		//	System.err.println("IOException: " + e.getMessage());
+		//}
+		
+	//}
+	textFile.createNewFile();
+	System.out.println("File has been created.");
+			
+	System.out.println("Keep or delete file? ");
 	String choice= input.next();
 	
-	if(choice.equalsIgnoreCase("D")) 
+	if(choice.equals("delete")) 
 	{
 		if(textFile.delete())
 		{
