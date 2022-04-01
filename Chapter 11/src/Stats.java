e7d82fd31bde494bfcfac9db4b1a7da9fd82904b
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-04-01 10:13:55 -0600

    pop

--- Chapter 11/src/Stats.java
+++ Chapter 11/src/Stats.java
@@ -1,4 +1,12 @@
+/*
+Program: Stats.java           Date of this Revision: 1-April-2022
 
+Purpose: read data from file and display data from file
+Author: Laura Frede, 904269504
+School: CHHS
+Course: Computer Science 30
+*/
+import java.io.*;
 public class Stats {
 
 	public static void main(String[] args) {
