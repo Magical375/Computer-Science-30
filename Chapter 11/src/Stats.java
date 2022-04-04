e5ddc60eac54ef2fb88a4d15e2fca29c8aa6cb55
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-04-04 11:21:33 -0600

    cccccccccc

--- Chapter 11/src/Stats.java
+++ Chapter 11/src/Stats.java
@@ -7,11 +7,42 @@ School: CHHS
 Course: Computer Science 30
 */
 import java.io.*;
+import java.util.Scanner;
 public class Stats 
 {
 public static void main(String[] args) 
 	{
+	textFile = new File (fileName);
 	
+	try {
+		in = new Filreader(textFile);
+		readFile = new BufferedReader(in);
+		masterAnswers = (String)readFile.readLine();
+		while ((studentName=readFile.reeadLine())!=null)
+		{
+			studentAnswers = readfile.readLine();
+			for(int grade =0; grade < masterAnswers.length(); grade++) 
+			{
+				if(studentAnswers.charAt(grade) == masterAnswers.charAt(grade))
+					//numCorrect +=1;
+			}
+		percentCorrect = (double)numCorrect/masterAnswers.length();
+		System.out.println(studentName + "\t" + pc.format9percentCorrect);
+		
+		}
+		readFile.close();
+		in.close();
+	}
+	catch(FileNotFoundException e) 
+	{
+	System.out.println("File does not exists.");
+	System.err.println("FileNotFoundException:"+ e.getMessage());
+	}
+	catch(IOException e) 
+	{
+	System.out.println("Can not read the file");
+	System.err.println("IOException:"+ e.getMessage());
+	}
 	}
 }
 /*
