6d4c97faefa224c218ce42b88fbb8cc268b6b553
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-04-05 09:30:55 -0600

    aaaaaaaaaaaaaa

---
+++ Chapter 11/src/ClassExample.java
@@ -0,0 +1,68 @@
+/*
+Program: Stats.java           Date of this Revision: 1-April-2022
+
+Purpose: read data from file and display data from file
+Author: Laura Frede, 904269504
+School: CHHS
+Course: Computer Science 30
+*/
+import java.io.*;
+import java.text.NumberFormat;
+import java.util.Scanner;
+public class ClassExample 
+{
+public static void main(String[] args) 
+	{
+	Scanner input = new Scanner(System.in);
+	
+	NumberFormat pc = NumberFormat.getPercentInstance();
+	File textFile;
+	FileReader in;
+	BufferedReader readFile;
+	String fileName, masterAnswers, studentAnswers, StudentName;
+	int numCorrect;
+	double percentCorrect;
+	
+	System.out.println("the name of the file?");
+	fileName =input.nextLine();
+	textFile = new File (fileName);
+	
+	try 
+	{
+		in = new FileReader(textFile);
+		readFile = new BufferedReader(in);
+		masterAnswers = (String)readFile.readLine();
+		while ((StudentName=readFile.readLine())!=null)
+		{
+			studentAnswers = readFile.readLine();
+			numCorrect = 0;
+			for(int grade =0; grade < masterAnswers.length(); grade++) 
+			{
+				if(studentAnswers.charAt(grade) == masterAnswers.charAt(grade))
+				{
+					numCorrect +=1;
+				}
+					
+			}
+		percentCorrect = (double)numCorrect/masterAnswers.length();
+		System.out.println(StudentName + "\t" + pc.format(percentCorrect));
+		
+		}
+		readFile.close();
+		in.close();
+	}
+	catch(FileNotFoundException e) 
+	{
+	System.out.println("File does not exists.");
+	System.err.println("FileNotFoundException:"+ e.getMessage());
+	}
+	catch(IOException e) 
+	{
+	System.out.println("Can not read the file");
+	System.err.println("IOException:"+ e.getMessage());
+	}
+	}
+}
+/*
+screen dump
+*/
--- Chapter 11/src/Stats.java
+++ Chapter 11/src/Stats.java
@@ -7,51 +7,13 @@ School: CHHS
 Course: Computer Science 30
 */
 import java.io.*;
+import java.text.NumberFormat;
 import java.util.Scanner;
 public class Stats 
 {
-public static void main(String[] args) 
+	public static void main(String[] args) 
 	{
+		Scanner input = new Scanner(System.in);
 	
-	
-	double perentCorrect;
-	
-	System.out.println("the name of the file?");
-	
-	textFile = new File (fileName);
-	
-	try {
-		in = new Filreader(textFile);
-		readFile = new BufferedReader(in);
-		masterAnswers = (String)readFile.readLine();
-		while ((studentName=readFile.readLine())!=null)
-		{
-			studentAnswers = readfile.readLine();
-			numCorrect = 0;
-			for(int grade =0; grade < masterAnswers.length(); grade++) 
-			{
-				if(studentAnswers.charAt(grade) == masterAnswers.charAt(grade))
-					//numCorrect +=1;
-			}
-		percentCorrect = (double)numCorrect/masterAnswers.length();
-		System.out.println(studentName + "\t" + pc.format(percentCorrect);
-		
-		}
-		readFile.close();
-		in.close();
-	}
-	catch(FileNotFoundException e) 
-	{
-	System.out.println("File does not exists.");
-	System.err.println("FileNotFoundException:"+ e.getMessage());
-	}
-	catch(IOException e) 
-	{
-	System.out.println("Can not read the file");
-	System.err.println("IOException:"+ e.getMessage());
-	}
 	}
 }
-/*
-screen dump
-*/
