4edafc9059b402dd7bd58e3db63c436fc017c863
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-04-05 09:30:59 -0600

    ssssssss

--- Chapter 11/src/ClassExample.java
+++ Chapter 11/src/ClassExample.java
@@ -1,11 +1,3 @@
-/*
-Program: Stats.java           Date of this Revision: 1-April-2022
-
-Purpose: read data from file and display data from file
-Author: Laura Frede, 904269504
-School: CHHS
-Course: Computer Science 30
-*/
 import java.io.*;
 import java.text.NumberFormat;
 import java.util.Scanner;
