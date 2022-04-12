f6cd83dedfcc226a47fa2620c6fc95019173a62f
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-04-12 09:59:47 -0600

    vvvvvvvvvvvvvv

--- Chapter 11/src/Roster.java
+++ Chapter 11/src/Roster.java
@@ -10,37 +10,40 @@ import java.io.*;
 import java.util.Scanner;
 public class Roster 
 {
-	public static void main(String[] args) 
+	public static void main(String Namef, String Namel) 
 	{
 		Scanner input = new Scanner(System.in);	
 		System.out.println("Please enter the directory of where you want the file with the name of the file you want to create.");
-		//     please use this directory         ../Chapter 11/src/StudentNames.txt
+		//     please use this directory:"../Chapter 11/src/StudentNames.txt"
 		String FileName= input.nextLine();//allows user to pick name of file
 		File textFile = new File(FileName);//creates the file with the name the user wanted
 		
-		System.out.println("Program will allow you to enter five students and their grades.");
+		System.out.println("How many students do you want to list?");
+		int num = input.nextInt();
 		
-		FileWriter out;//enables the write to file
-		File dataFile;
+		//FileWriter out;//enables the write to file
 		BufferedWriter writeFile;
 		
 		try 
 		{
-			dataFile = new File(FileName);
-			out = new FileWriter(dataFile);	
-			writeFile = new BufferedWriter(out);
-		for(int i = 0; i<5; i++) 
+			FileOutputStream out = new FileOutputStream(textFile);
+			ObjectOutputStream writetextFile = new ObjectOutputStream(out);
+			//textFile = new File(FileName);
+			//out = new FileWriter(textFile);	
+			//writeFile = new BufferedWriter(out);
+		for(int i = 0; i<num; i++) 
 			{
 			System.out.println("Please enter the first name of a student");
-			String Namef= input.next();
+			Namef= input.next();
 			System.out.println("Please enter the last name of a student");
-			String Namel= input.next();
-			writeFile.write(Namef);//writes information the user entered
-			writeFile.write(Namel);
-			writeFile.newLine();
+			Namel= input.next();
+			writetextFile.write(StuName(Namef, Namel));//writes information the user entered	
 			
 			}
+		writetextFile.close();
+		System.out.println("program has writen student names");
 		}
+			
 			catch(IOException e)
 				{
 				System.out.println("There is a problem writting to file.");
@@ -59,6 +62,7 @@ public class Roster
 			
 			while ((StudentName=readFile.readLine())!=null)//the program reads file for student names
 			{	
+				
 				System.out.println(StudentName);//displays student names
 			}
 			readFile.close();
---
+++ Chapter 11/src/stuName.java
@@ -0,0 +1,12 @@
+
+public class stuName extends Roster
+{
+
+	public String stuName()
+		{
+		super(Sarah, Smith);	
+		String stuString;
+			stuString = stuName + "Name: "+ Namef +" "+Namel;
+			return(stuString);
+		}
+}
