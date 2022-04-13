149864fc4c6a5db25755fb51fe031098d4ad36f2
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-04-13 10:45:16 -0600

    uuuuuuuuuuuuu

--- Chapter 11/src/stuName.java
+++ Chapter 11/src/stuName.java
@@ -1,12 +1,18 @@
+/*
+Program: Roster.java           Date of this Revision: 13-April-2022
 
+Purpose:holds first and last name of students in roster program
+Author: Laura Frede, 904269504
+School: CHHS
+Course: Computer Science 30
+*/
 public class stuName extends Roster
 {
-
-	public String stuName()
+	public String stuName()//a to string constructor 
 		{
-		super(Sarah, Smith);	
-		String stuString;
-			stuString = stuName + "Name: "+ Namef +" "+Namel;
-			return(stuString);
+		super(Sarah, Smith);//gets variables from supperclass roster	
+		String stuString;//declares string with name
+			stuString = stuName + "Name: "+ Namef +" "+Namel;//initalizes sting with student's first and last names
+			return(stuString);//returns string
 		}
 }
