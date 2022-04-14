675485245b3fc8d46393c2bde1e5a9fe2ccccd54
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-04-14 10:26:24 -0600

    dddddddddddddddd

--- Chapter 11/src/FindAndReplace.java
+++ Chapter 11/src/FindAndReplace.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.Scanner;
@@ -17,22 +20,47 @@ public class FindAndReplace {
 
 	public static void main(String[] args) 
 	{
+		FileReader in;
+		BufferedReader readFile;
+		String FileName, lineoftext;
+		File newFile;
+		BufferedWriter writeFile;
+		FileWriter out;
+		
 		Scanner input = new Scanner(System.in);	
 		System.out.println("Please enter the directory of where the file is and the name of the file.");
 		//     please use this directory:"../Chapter 11/src/Replace Version Two.txt"
-		String FileName= input.nextLine();//holds name of file entered
+		FileName= input.nextLine();//holds name of file entered
+		File textFile = new File(FileName);
 		System.out.println("Please enter what you want to replace.");
 		String current= input.nextLine();
 		System.out.println("Please enter what you want to replace it with.");
 		String replaced= input.nextLine();
 		
+			
 		try 
 		{
 		//FileInputStream inn = new FileInputStream(FileName);//allows the program to read the file
 		//ObjectInputStream readtext = new ObjectInputStream(inn);
-		
-		in = new FileReader(FileName);
+			
+			
+					
+		in = new FileReader(textFile);
 		readFile = new BufferedReader(in);
+		FileName = FileName.replace(".", "NEW");
+		newFile = new File(FileName);
+		
+		out = new FileWriter(newFile);	
+		writeFile = new BufferedWriter(out);
+		
+		while((lineoftext=readFile.readLine())!=null)
+	{
+	lineoftext = lineoftext.replaceAll(current, replaced);
+	writeFile write lineoftext;
+	}
+			
+		
+		
 		//System.out.println((FileName) readtext.readObject() );//displays the current file
 
 		
