ea7d0e920daf5afd3c1f625368828b5fd9497fba
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-04-14 10:07:43 -0600

    lllllllllllllllllllllllllllllllll

--- Chapter 11/src/FindAndReplace.java
+++ Chapter 11/src/FindAndReplace.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.Scanner;
@@ -19,14 +21,18 @@ public class FindAndReplace {
 		System.out.println("Please enter the directory of where the file is and the name of the file.");
 		//     please use this directory:"../Chapter 11/src/Replace Version Two.txt"
 		String FileName= input.nextLine();//holds name of file entered
+		System.out.println("Please enter what you want to replace.");
+		String current= input.nextLine();
+		System.out.println("Please enter what you want to replace it with.");
+		String replaced= input.nextLine();
 		
 		try 
 		{
-		FileInputStream inn = new FileInputStream(FileName);//allows the program to read the file
-		ObjectInputStream readtext = new ObjectInputStream(inn);
+		//FileInputStream inn = new FileInputStream(FileName);//allows the program to read the file
+		//ObjectInputStream readtext = new ObjectInputStream(inn);
 		
-		//int num;
-		//String objectToMatch, blank;
+		in = new FileReader(textFile);
+		readFile = new BufferedReader(in);
 		System.out.println((FileName) readtext.readObject() );//displays the current file
 
 		
