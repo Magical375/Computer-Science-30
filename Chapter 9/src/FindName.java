db28efc6a32d310ece56d9ec30bc62633e862a48
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-04-22 10:29:54 -0600

    0oooooooooooooooo

---
+++ Chapter 9/src/FindName.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class FindName 
+{
+	public static void main(String[] args) 
+	{	
+		Scanner input = new Scanner(System.in);	
+		System.out.println("Please enter how many names you want to store");//allows user to enter the number of names to store
+		int num = input.nextInt();
+		String [] nameslist = new String [num];
+		String name;
+		for(int i = 0; i<num; i++)//runs loop for the exact number of names the user wants 
+		{
+			System.out.print("Enter a name: ");
+			name = input.nextLine();	
+		}
+	}
+}
+
+
