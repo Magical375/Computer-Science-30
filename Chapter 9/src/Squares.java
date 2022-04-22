285e55df7456ff0fc994219e40bcf5290eb771f2
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-04-22 10:07:45 -0600

    hhhhhhhhhhhhhhh

---
+++ Chapter 9/src/Squares.java
@@ -0,0 +1,18 @@
+
+public class Squares 
+{
+	public static void main(String[] args) 
+	{
+		int [] squares = new int [5];//store array named squares 
+		for(int i= 0; i < squares.length; i++)
+		{
+		squares [i] = i*i;
+		}
+		//display the square numbers  
+		for(int i=0; i< squares.length; i++)
+		{
+			System.out.println(squares[i]);
+		}
+	}
+
+}
