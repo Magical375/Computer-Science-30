7d9e002a67da29f45faecba58cbd94d6a02cf68e
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-05-05 09:57:00 -0600

    ttttttttttttttt

--- Chapter 9/src/HighestGrade.java
+++ Chapter 9/src/HighestGrade.java
@@ -17,13 +17,46 @@ public class HighestGrade {
 		Double grade;
 		for(int i = 0; i<5; i++)//runs loop for five grades 
 		{
-			System.out.print("Enter a grade: ");
+			System.out.println("Enter a grade: ");
 			grade = input.nextDouble();
 			gradeslist[i] = grade;
 		}
 		input.close();//closes the scanner, or the editor program is annoyed
-		Double find= 0.0;
-		int highestgrade= SearchDouble.linear(gradeslist, find);
+		Double element0, element1, element2, element3, element4;
+		element0 = gradeslist.get(0);
+		element1 = gradeslist.get(1);
+		//compare double values
+		if(element0.compareTo(element1)>0)
+		{
+			System.out.println(element0+"is greater than"+element1);
+		}else if (element0.compareTo(element2)>0)
+		{
+			System.out.println(element0+"is greater than"+element2);
+		}else if (element0.compareTo(element3)>0)
+		{
+			System.out.println(element0+" is greater than "+element3);
+		}else if (element0.compareTo(element4)>0)
+		{
+			System.out.println(element0+" is greater than "+element4);
+		}else if (element1.compareTo(element2)>0)
+		{
+			System.out.println(element1+" is greater than "+element2);
+		}else if (element1.compareTo(element3)>0)
+		{
+			System.out.println(element1+" is greater than "+element3);
+		}else if (element1.compareTo(element4)>0)
+		{
+			System.out.println(element1+" is greater than "+element4);
+		}else if (element2.compareTo(element3)>0)
+		{
+			System.out.println(element2+" is greater than "+element3);
+		}else if (element2.compareTo(element4)>0)
+		{
+			System.out.println(element2+" is greater than "+element4);
+		}else if (element3.compareTo(element4)>0)
+		{
+			System.out.println(element3+" is greater than "+element4);
+		}
 	}
 
 }
