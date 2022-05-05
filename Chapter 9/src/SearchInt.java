4ed697210d4e30ec24f745a1ba61817a7bf3a3bd
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-05-05 09:09:44 -0600

    ttttttttttttt

--- Chapter 9/src/SearchInt.java
+++ Chapter 9/src/SearchInt.java
@@ -1,26 +1,19 @@
-/*
- * Search.java
- */
- 
- public class SearchInt {
 
-	/**
-	 * Returns the index of the element numToFind in array array.
-	 * -1 returned if element not found.
-	 * pre: none
-	 * post: index of numToFind has been returned. -1 has been
-	 * returned if element not found.
-	 */
-	public static int linear(int[] array, int numToFind) {
+ public class SearchInt 
+ {
+	public static int linear(int[] array, int numToFind) 
+	{
 		int index = 0;
-		
-		while ((array[index] != numToFind) && (index < array.length - 1)) {
+		while ((array[index] != numToFind) && (index < array.length - 1)) 
+		{
 			index += 1;
 		}
 		
-		if (array[index] == numToFind) {
+		if (array[index] == numToFind) 
+		{
 			return(index);
-		} else {
+		} else 
+		{
 			return(-1);
 		}
 	}
