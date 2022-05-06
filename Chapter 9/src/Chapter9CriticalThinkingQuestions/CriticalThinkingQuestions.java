f8623f0a9923aa4fc83c537f241c005024dfd87b
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-05-06 10:37:27 -0600

    aaaaaaaaaaaa

---
+++ Chapter 9/src/Chapter9CriticalThinkingQuestions/CriticalThinkingQuestions.java
@@ -0,0 +1,12 @@
+package Chapter9CriticalThinkingQuestions;
+
+public class CriticalThinkingQuestions 
+{
+
+/*
+1 what index value does the third element of an array have? 2
+2 Write the declaration for an array named quantities that stores 20 integers. 
+int max = 20;
+int[] quantities = new int[max]; 
+ */
+}
