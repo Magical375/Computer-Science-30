219c488bd002055b6cd43068c04387ea87c32195
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-05-09 11:48:22 -0600

    Done

--- Chapter 9/src/Chapter9CriticalThinkingQuestions/CriticalThinkingQuestions.java
+++ Chapter 9/src/Chapter9CriticalThinkingQuestions/CriticalThinkingQuestions.java
@@ -6,10 +6,36 @@ public class CriticalThinkingQuestions
 /*
 1 what index value does the third element of an array have? 2
 2 Write the declaration for an array named quantities that stores 20 integers. 
-int max = 20;
-int[] quantities = new int[max]; 
+int[] quantities = new int[20]; 
 3 Write a declaration for an array named heights storing the numbers 1.65,2.15,and 4.96
-double [] heights = {"1.65","2.15,"4.96"}
+double [] heights = {"1.65","2.15","4.96"}
+4 Write a for-each statement that displays the integer values stored in an array named grades. 
+for(string element : grades)
+{
+System.out.println(element);
+}
+5 write an algorithm for inserting data into an array so that existing data is moved up one position to make room for the new data. 
+add(int 0, object element);
+5b Write an algorithm for deleting data from an array so that existing data is moved to close the gap made by the deleted data. 
+ArrrayList<String> myStrings = new ArrayList<String>();
+myStyrings.add("Bob");
+myStrings.add("Sarah");
+myStrings.add("Steve");
+myStrings.remove(1);
+for (String name : myStrings){
+System.out.println(name);}
+displays 
+Bob 
+Steve
+6 How does passing an entire array to a method differ from passing a single element of the array?
+passing a whole array to a method passes the reference to the elements, allowing the method to access an element of the array and change its value. 
+passing a single element passes only the data stored and not a reference to the data location.
+7 why are offset array indexes required in some cases?
+To store something directly corresponding to their outcome. 
+8 What output is displayed by the statements below?
+String name = "Elaine";
+System.out.println(name.charAt(3));
+displays i
 
  */
 }
