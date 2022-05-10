c4a37b1f8234df68d0082c7c96eeacee677dd056
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-05-10 09:35:54 -0600

    ssssssssssss

---
+++ Phidget/src/aaaaaaaaaaa.java
@@ -0,0 +1,17 @@
+import com.phidget22.*;
+public class aaaaaaaaaaa 
+{
+	public static void main(String[] args) throws Exception{
+        //Create
+        VoltageRatioInput scale = new VoltageRatioInput();
+        
+        //Open
+        scale.open(1000);
+        
+        //Use your Phidgets
+        while(true){
+            System.out.println("Offset Value: " + scale.getVoltageRatio());
+            Thread.sleep(250);
+        }
+    }    
+}
