0c41a15d6bab64fccf4cb954660cc7100e4fe0bd
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-05-13 10:37:51 -0600

    aaaaaaaaaaaaaaa

--- Stupidthing/src/scale.java
+++ Stupidthing/src/scale.java
@@ -9,12 +9,23 @@ public class scale
         //Open
         scale.open(1000);
         
+        //Use your Offset Value
+        double offsetValue =-7.53952190631047E-6;
+        
         //Use your Phidgets
         while(true)
         {
-            System.out.println("Offset Value: " + scale.getVoltageRatio());
-            Thread.sleep(250);
+        //Calculate Weight (kg)
+        //double weight = 4700 * (scale.getVoltageRatio() - offsetValue);
+        
+        double weight = -23286* (scale.getVoltageRatio() - offsetValue);
+            
+        //Display Weight
+        System.out.println(String.format("%.3f kg", weight));    
+        	
+        //System.out.println("Offset Value: " + scale.getVoltageRatio());
+        Thread.sleep(250);
         }
     }    
 }
-// Offset Value: -8.596805859635028E-5
+
