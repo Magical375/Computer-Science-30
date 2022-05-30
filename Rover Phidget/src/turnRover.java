9418dfdda2c7dd23cc617badb3dd7bfc00b82e68
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-05-30 10:39:27 -0600

    aaaaaaaaaaaaaaaaaaa

--- Rover Phidget/src/turnRover.java
+++ Rover Phidget/src/turnRover.java
@@ -29,22 +29,18 @@ public class turnRover
         rightMotors.open(5000);
 
        
-        	//leftMotors.setTargetVelocity(1); rightMotors.setTargetVelocity(-1);//turns clockwise	
-        	leftMotors.setTargetVelocity(-1); rightMotors.setTargetVelocity(1);//turns counter-clockwise
+        //leftMotors.setTargetVelocity(1); rightMotors.setTargetVelocity(-1);//turns clockwise	
+        leftMotors.setTargetVelocity(-1); rightMotors.setTargetVelocity(1);//turns counter-clockwise
         
         //goes forward turns around and goes back to start 
         	
-  /* 
         leftMotors.setTargetVelocity(1);rightMotors.setTargetVelocity(1);
         Thread.sleep(1000);
         
         leftMotors.setTargetVelocity(1); rightMotors.setTargetVelocity(-1);
-        Thread.sleep(1125);
+        Thread.sleep(1140);
         
         leftMotors.setTargetVelocity(1);rightMotors.setTargetVelocity(1);
-        
-  */
-        
         
         //Wait for 1 second
         Thread.sleep(1400);
