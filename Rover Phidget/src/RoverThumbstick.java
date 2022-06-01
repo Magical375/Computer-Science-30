f74afad911a20fa22ab4e1b62da1df225dc5c098
904269504 <904269504@L815WD220008.edu.cbe.ab.ca>
2022-06-01 11:55:52 -0600

    ssssssssssssssssssssssss

---
+++ Rover Phidget/src/RoverThumbstick.java
@@ -0,0 +1,20 @@
+/*
+Program: RoverThumbstick.java           Date of this Revision: 1-June-2022
+
+Purpose: controls the rover with the thumbstick
+
+Author: Laura Frede, 904269504
+School: CHHS
+Course: Computer Science 30
+*/  
+//Add Phidgets Library
+import com.phidget22.*;
+public class RoverThumbstick 
+{
+	public static void main(String[] args) throws Exception 
+	{
+		
+
+	}
+
+}
